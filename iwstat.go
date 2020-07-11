f5d9ee3d9a42a3ecf256d28adc94c4a6ffe857d2
rohan-prasad <28514947+rohan-prasad@users.noreply.github.com>
2020-07-11 16:22:12 -0700

    commit on master

---
+++ iwstat.go
@@ -0,0 +1,53 @@
+//Package iwstat provides a parser for OpenWRT iwinfo stats
+package iwstat
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+	"strings"
+)
+
+//IWStat statistics of associated clients
+type IWStat struct {
+	MAC string
+
+	RSSI int
+}
+
+func scan(r io.Reader) ([]IWStat, error) {
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+
+	var stats []IWStat
+	for s.Scan() {
+
+		const nFields = 34
+		fields := strings.Fields(string(s.Bytes()))
+		if len(fields) != nFields {
+			continue
+		}
+		var times [34]int
+		for i, idx := range []int{1, 2} {
+			v, err := strconv.Atoi(fields[idx])
+			if err != nil {
+				return nil, err
+
+			}
+			times[i] = v
+		}
+
+		stats = append(stats, IWStat{
+			MAC:  fields[0],
+			RSSI: times[0],
+		})
+	}
+
+	if err := s.Err(); err != nil {
+		return nil, err
+
+	}
+
+	return stats, nil
+}
