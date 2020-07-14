3c512f4487336b7450d2de91869f12ed0bf57fb6
rohan-prasad <28514947+rohan-prasad@users.noreply.github.com>
2020-07-13 21:20:00 -0700

    Update iwstat.go

--- iwstat.go
+++ iwstat.go
@@ -10,8 +10,27 @@ import (
 
 //IWStat statistics of associated clients
 type IWStat struct {
-	MAC  string
-	RSSI int
+	ifname             string
+	mac                string
+	rssi               int
+	snr                int
+	clientInactive     int
+	rxPhy              int
+	rxMbytes           int
+	rxPrr              int
+	rxVhtMcsIndex      int
+	rxVhtMcsMhz        int
+	rxVhtNss           int
+	rxPackets          int
+	txPhy              int
+	txMbytes           int
+	txPrr              int
+	txVhtMcsIndex      int
+	txVhtMcsMhz        int
+	txVhtNss           int
+	txPackets          int
+	expectedThroughput int
+	channelUtlization  int
 }
 
 //Scan reads and parses iwinfo
@@ -29,7 +48,7 @@ func Scan(r io.Reader) ([]IWStat, error) {
 			continue
 		}
 		var times [20]int
-		for i, idx := range []int{1,2} {
+		for i, idx := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20} {
 			v, err := strconv.Atoi(fields[idx])
 			if err != nil {
 				return nil, err
