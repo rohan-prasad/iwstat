72a60910b2b279fdaa98022e7bbd935f66c5bc2d
rohan-prasad <28514947+rohan-prasad@users.noreply.github.com>
2020-07-14 00:04:52 -0700

    .

--- iwstat.go
+++ iwstat.go
@@ -24,7 +24,7 @@ func Scan(r io.Reader) ([]IWStat, error) {
 	var stats []IWStat
 	for s.Scan() {
 
-		const nFields = 21
+		const nFields = 22
 		fields := strings.Fields(string(s.Bytes()))
 		if len(fields) != nFields {
 			continue
