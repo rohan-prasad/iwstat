b12d12bf322908af15b39bc025eb054700f4f8c0
rohan-prasad <28514947+rohan-prasad@users.noreply.github.com>
2020-07-13 22:43:20 -0700

    commit on master

--- iwstat.go
+++ iwstat.go
@@ -48,7 +48,7 @@ func Scan(r io.Reader) ([]IWStat, error) {
 			continue
 		}
 		var times [20]int
-		for i, idx := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20} {
+		for i, idx := range []int{1: 20} {
 			v, err := strconv.Atoi(fields[idx])
 			if err != nil {
 				return nil, err
